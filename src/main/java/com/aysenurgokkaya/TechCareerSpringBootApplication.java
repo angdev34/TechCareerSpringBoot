771f53aeeb98d1413c73931259189ebf2d65d70d
Ayşe Nur Gökkaya <34404993+aysenurgokkaya@users.noreply.github.com>
2022-04-13 13:03:28 +0300

    first commit

---
+++ src/main/java/com/aysenurgokkaya/TechCareerSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.aysenurgokkaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechCareerSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechCareerSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aysenurgokkaya/TechCareerSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aysenurgokkaya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechCareerSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
